80a5dbb1678fce5f43b131f9e4ad1569f85845bf
Anh Dinh <anh.dinh@ktek.io>
2020-05-12 17:26:58 +0700

    - update

--- Build.js
+++ Build.js
@@ -1,3 +1,8 @@
+if (process.argv.length > 2)
+{
+    console.log(process.argv);
+}
+
 const {execSync} = require("child_process");
 
 execSync(`/Applications/CocosCreator.app/Contents/MacOS/CocosCreator --path ./ --build "platform=web-mobile;debug=false"`, { stdio: 'inherit' });
